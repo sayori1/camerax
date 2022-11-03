c65ff3afc7fb7b4855fc6680a76034ff9cd708c5
sayori1 <ridickeon@yandex.com>
2022-11-03 14:12:00 +0500

    add face model

---
+++ lib/camerax.dart
@@ -0,0 +1,7 @@
+library camerax;
+
+export 'src/camera_args.dart';
+export 'src/camera_controller.dart';
+export 'src/camera_facing.dart';
+export 'src/camera_view.dart';
+export 'src/torch_state.dart';
